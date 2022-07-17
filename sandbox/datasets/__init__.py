c547018ca9f711ce602d93977381950a1cb077ed
sndpgm <108000555+sndpgm@users.noreply.github.com>
2022-07-17 22:16:47 +0900

    CLN: decide import scope in datasets.

--- sandbox/datasets/__init__.py
+++ sandbox/datasets/__init__.py
@@ -1,1 +1,12 @@
 """Datasets module."""
+from . import air_passengers, blsallfood, hakusan, maxtemp, mye1f, nikkei225, whard
+
+__all__ = [
+    "air_passengers",
+    "blsallfood",
+    "hakusan",
+    "maxtemp",
+    "mye1f",
+    "nikkei225",
+    "whard",
+]
