eb6d7de9291c309939680c5a6e9a1eb157af4fa0
sndpgm <108000555+sndpgm@users.noreply.github.com>
2022-07-29 13:09:31 +0900

    DATASET: add road traffic fatalities in Finland and Norway.

---
+++ sandbox/datasets/rtf/__init__.py
@@ -0,0 +1,4 @@
+"""Datasets road traffic fatalities in Norway and Finland."""
+__all__ = ["load", "COPYRIGHT", "TITLE", "DESCRIPTION", "SOURCE", "NOTE"]
+
+from .data import COPYRIGHT, DESCRIPTION, NOTE, SOURCE, TITLE, load
