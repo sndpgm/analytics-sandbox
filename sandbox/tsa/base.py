8ce59d7ca31c78758f84e20a325f6291d2d85c2b
sndpgm <108000555+sndpgm@users.noreply.github.com>
2022-07-31 06:52:59 +0900

    ENH: update base class on tsa and add linear gaussian state space model.

--- sandbox/tsa/base.py
+++ sandbox/tsa/base.py
@@ -1,37 +1,37 @@
+"""Base classes for time series estimators."""
 import functools
 from abc import ABCMeta, abstractmethod
 
 from sklearn.base import BaseEstimator
 
-from sandbox.utils.validation import check_X_y
+from sandbox.metrics import score
 
 
 class _BaseModelMeta(ABCMeta):
-    def __new__(cls, name, bases, attrs):
-        new_class = super().__new__(cls, name, bases, attrs)
+    """Meta class for model estimators."""
+
+    def __new__(mcs, name, bases, attrs):
+        new_class = super().__new__(mcs, name, bases, attrs)
 
         # fit / predict methods are wrapped.
-        new_class.fit = cls.wrapper_fit(new_class.fit)
-        new_class.predict = cls.wrapper_predict(new_class.predict)
+        new_class.fit = mcs.wrapper_fit(new_class.fit)
+        new_class.predict = mcs.wrapper_predict(new_class.predict)
 
         return new_class
 
     @classmethod
-    def wrapper_fit(cls, child_fit):
+    def wrapper_fit(mcs, child_fit):
         """Common pre-/post-process in fit method."""
 
         @functools.wraps(child_fit)
         def _wrapped(self, X, y=None, **kwargs):
-            if X is not None and y is None:
-                X, y = y, X
-            X, y = check_X_y(X, y)
             child_fit(self, X=X, y=y, **kwargs)
             return self
 
         return _wrapped
 
     @classmethod
-    def wrapper_predict(cls, child_predict):
+    def wrapper_predict(mcs, child_predict):
         """Common pre-/post-process in predict method."""
 
         @functools.wraps(child_predict)
@@ -46,17 +46,26 @@ class BaseTimeSeriesModel(BaseEstimator, metaclass=_BaseModelMeta):
     """Base class for time series model."""
 
     @abstractmethod
-    def fit(self, X, y=None, **kwargs):
+    def fit(self, X, y=None):
         """Fit time series model."""
+        pass
 
     @abstractmethod
-    def predict(self, X, **kwargs):
+    def predict(self, X):
         """Predict using time series model."""
+        pass
 
     def conf_int(self, X, alpha):
         """Construct confidence interval for the fitted parameters."""
         pass
 
-    def score(self, X, y):
+    def score(self, X, y, scorer="r2", **kwargs):
         """Return the coefficient of determination of the prediction."""
-        pass
+        y_pred = self.predict(X)
+        if scorer == "r2":
+            return score.r2_score(y_true=y, y_pred=y_pred, **kwargs)
+        elif scorer == "mape":
+            return score.mape_score(y_true=y, y_pred=y_pred, **kwargs)
+        else:
+            msg = "Specified `scorer` does not support."
+            raise ValueError(msg)
